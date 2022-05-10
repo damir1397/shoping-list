1ec204908990e1413ab8e8a2058b591cb526235d
Damir <damir.danzo@gmail.com>
2022-05-10 18:59:32 +0600

    create package

--- app/src/main/java/kg/damir/shoping_list/presentation/MainActivity.kt
+++ app/src/main/java/kg/damir/shoping_list/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package kg.damir.shoping_list
+package kg.damir.shoping_list.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kg.damir.shoping_list.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
