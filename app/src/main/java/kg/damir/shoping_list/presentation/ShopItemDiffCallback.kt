f01b6ab268383268cee3120b7a3d4bbdcde6d5ab
Damir <damir.danzo@gmail.com>
2022-05-16 16:54:29 +0600

    ShopItemDiffCallback изменения на ListAdapter вынесение класса ShopItemViewHolder

---
+++ app/src/main/java/kg/damir/shoping_list/presentation/ShopItemDiffCallback.kt
@@ -0,0 +1,14 @@
+package kg.damir.shoping_list.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import kg.damir.shoping_list.domain.ShopItem
+
+class ShopItemDiffCallback : DiffUtil.ItemCallback<ShopItem>() {
+    override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem == newItem
+    }
+}
