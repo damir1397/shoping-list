399d6d854d87eb37581ad8501135f50e57a14584
Damir <damir.danzo@gmail.com>
2022-05-17 10:43:30 +0600

    Добавлен второй экран

---
+++ app/src/main/java/kg/damir/shoping_list/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package kg.damir.shoping_list.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kg.damir.shoping_list.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
